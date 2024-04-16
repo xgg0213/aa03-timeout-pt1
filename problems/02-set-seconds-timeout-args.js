70521176513de7fe13d9e3d46cbc3afd09b7c904
Andrew <andrew.lizon12@gmail.com>
2024-04-15 20:41:40 -0700

    Completed Problems 1-2 Driver:Andrew/Nav:Erin

--- problems/02-set-seconds-timeout-args.js
+++ problems/02-set-seconds-timeout-args.js
@@ -23,8 +23,14 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  // Your code here 
+setTimeout(cb(...args), delayInSeconds * 1000)
+
 }
+
+// function printSum(num1, num2, num3) {
+//   console.log(num1 + num2 + num3);
+// }
+// setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
