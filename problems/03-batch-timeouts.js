d74381d709988c0f4656082fca81bb23144ebda0
Andrew <andrew.lizon12@gmail.com>
2024-04-15 21:24:03 -0700

    Solved Problem and Solving Problem 4

--- problems/03-batch-timeouts.js
+++ problems/03-batch-timeouts.js
@@ -33,9 +33,30 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-  // Your code here 
+let arr = [];
+  for (let i = 0; i < callbacks.length; i++) {
+let el = callbacks[i];
+let el2 = delays[i];
+arr.push(setTimeout(el, el2));
+  }
+return arr;
 }
 
+
+const sayHello = () => console.log('hi');
+const sayGoodbye = () => console.log('bye');
+const shout = () => console.log('WHAT?');
+const tasks = [sayHello, sayGoodbye, shout];
+const delays = [500, 200, 900];
+
+const timeoutObjs = batchTimeouts(tasks, delays);
+
+// should print: 
+//  'bye' after 200 ms
+//  'hi' after 500 ms
+//  'WHAT?' after 900 ms
+
+console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = batchTimeouts;
--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -17,9 +17,20 @@ intervalCount(function() {
 ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here 
+function intervalWrap() {
+while (amount > 0) {
+  setInterval(cb, delay);
+  amount--;
 }
 
+}
+  clearInterval(intervalWrap)
+}
+
+
+intervalCount(function() {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = intervalCount;
